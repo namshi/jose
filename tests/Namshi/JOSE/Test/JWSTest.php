1a0e9b43698a7241a6cb3d8e888feb3e1528f7ec
Ayham Alzoubi <Ayham.a.Alzoubi@gmail.com>
2013-09-19 16:11:24 +0400

    adding the 'exp' to validate the expiration date of the JWS

--- tests/Namshi/JOSE/Test/JWSTest.php
+++ tests/Namshi/JOSE/Test/JWSTest.php
@@ -4,6 +4,7 @@ namespace Namshi\JOSE\Test;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use Namshi\JOSE\JWS;
+use DateTime;
 
 class JWSTest extends TestCase
 {
@@ -11,7 +12,11 @@ class JWSTest extends TestCase
     
     public function setup()
     {
-        $data       = array('a' => 'b');
+        $date       = new DateTime('tomorrow');
+        $data       = array(
+            'a'     => 'b',
+            'exp'   => $date->format(DateTime::ISO8601)
+        );
         $this->jws  = new JWS('RS256');
         $this->jws->setPayload($data);
     }
@@ -27,6 +32,30 @@ class JWSTest extends TestCase
         
         $payload = $jws->getPayload();
         $this->assertEquals('b', $payload['a']);
+    }
+
+    public function testValidationOfAValidJWS()
+    {
+        $privateKey = openssl_pkey_get_private(SSL_KEYS_PATH . "private.key", self::SSL_KEY_PASSPHRASE);
+        $this->jws->sign($privateKey);
+
+        $jws        = JWS::load($this->jws->getTokenString());
+        $public_key = openssl_pkey_get_public(SSL_KEYS_PATH . "public.key");
+        $this->assertTrue($jws->isValid($public_key));
+    }
+
+    public function testValidationOfInvalidJWS()
+    {
+        $date       = new DateTime('yesterday');
+        $this->jws->setPayload(array(
+            'exp' => $date->format(DateTime::ISO8601)
+        ));
+        $privateKey = openssl_pkey_get_private(SSL_KEYS_PATH . "private.key", self::SSL_KEY_PASSPHRASE);
+        $this->jws->sign($privateKey);
+
+        $jws        = JWS::load($this->jws->getTokenString());
+        $public_key = openssl_pkey_get_public(SSL_KEYS_PATH . "public.key");
+        $this->assertFalse($jws->isValid($public_key));
     }
     
     public function testVerificationThatTheJWSIsSigned()
